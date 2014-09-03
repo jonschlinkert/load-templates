d54677dbc7a32eec97ce900fef66342b8008a9f1
jonschlinkert <dev@sellside.com>
2014-09-02 21:32:23 -0400

    update docs, run verb

--- README.md
+++ README.md
@@ -2,12 +2,134 @@
 
 > Load templates from file paths, globs or objects, and cache them as normalized objects.
 
+This is a template loader, pure and simple, no rendering or caching.
+
+Currently any of the following input configurations will return a normalized template object with the same root properties:
+
+```js
+loader.load(['abc/*.hbs']);
+loader.load('abc/*.md');
+loader.load({'abc/def.md': {content: 'ghi.', lmn: 'xyz'});
+loader.load({'abc/def.md': {content: 'ghi.'}}, {lmn: 'xyz'});
+loader.load({path: 'abc/def.md', content: 'ghi.'}, {lmn: 'xyz'});
+loader.load({path: 'abc/def.md', content: 'ghi.', lmn: 'xyz'});
+loader.load('abc.md', 'def <%= name %>.');
+loader.load('abc.md', 'def <%= name %>.', {name: 'Jon Schlinkert'});
+```
+
+**Root properties**
+
+* `path`: file path or key to use for the template.
+* `data`: data from the parsed template, e.g. front-matter.
+* `locals`: locals pass on one of the loader methods. (keeping locals and data seperate allows you to merge however you need to.)
+* `content`: the parsed content of the template
+* `orig`: the original content of the template, if parsed
+
+Properties that are not on this list will be moved/copied to the `data` object and retained on `orig`.
+
 ## Install
 #### Install with [npm](npmjs.org):
 
 ```bash
 npm i load-templates --save-dev
 ```
+
+## Run tests
+
+```bash
+npm test
+```
+
+## API
+### [loader](index.js#L30)
+
+* `options` **{Object}**: Options to initialize `loader` with    
+
+```js
+var loader = require('load-templates');
+```
+
+### [.load](index.js#L86)
+
+Expand glob patterns, load, read, parse and normalize files from file paths, strings, objects, or arrays of these types.
+
+* `key` **{String|Object|Array}**: Array, object, string or file paths.    
+* `value` **{String|Object}**: String of content, `file` object with `content` property, or `locals` if the first arg is a file path.    
+* `options` **{Object}**: Options or `locals`.    
+* `returns` **{Object}**: Normalized file object.  
+
+**Examples:**
+
+Filepaths or arrays of glob patterns.
+
+```js
+var temlates = loader.load(['pages/*.hbs']);
+var posts = loader.load('posts/*.md');
+```
+
+As strings or objects:
+
+```js
+// loader.load(key, value, locals);
+var docs = loader.load({'foo/bar.md': {content: 'this is content.'}}, {foo: 'bar'});
+
+// loader.load(key, value, locals);
+var post = loader.load('abc.md', 'My name is <%= name %>.', {name: 'Jon Schlinkert'});
+```
+
+### [.string](index.js#L105)
+
+* `key` **{String}**: Glob patterns or file paths.    
+* `value` **{String|Object}**: String of content, `file` object with `content` property, or `locals` if the first arg is a file path.    
+* `options` **{Object}**: Options or `locals`.    
+* `returns` **{Object}**: Normalized file object.  
+
+Expand glob patterns, load, read, parse and normalize files
+from file paths or strings.
+
+### [.array](index.js#L144)
+
+* `patterns` **{Object}**: Glob patterns or array of filepaths.    
+* `options` **{Object}**: Options or `locals`    
+* `returns` **{Array}**: Array of normalized file objects.  
+
+Normalize an array of patterns.
+
+### [.object](index.js#L161)
+
+* `file` **{Object}**: The object to normalize.    
+* `options` **{Object}**: Options or `locals`    
+
+Normalize a template object.
+
+### [._cwd](index.js#L173)
+
+* `filepath` **{String}**    
+
+The current working directory to use. Default is `process.cwd()`.
+
+### [.rename](index.js#L187)
+
+* `filepath` **{String}**    
+
+Rename the `key` of each template loaded using whatever rename function
+is defined on the options. `path.basename` is the default.
+
+### [.parse](index.js#L204)
+
+* `filepath` **{String}**: The path of the file to read/parse.    
+* `Options` **{Object}**: Options or `locals`.    
+
+Parse the content of each template loaded using whatever parsing function
+is defined on the options. `fs.readFileSync` is used by default.
+
+### [.normalize](index.js#L233)
+
+* `file` **{Object}**: The template object to normalize.    
+* `Options` **{Object}**: Options or `locals`.    
+
+Normalize a template using whatever normalize function is
+defined on the options.
 
 ## Author
 
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Module dependencies
+ */
 
 var _ = require('lodash');
 var fs = require('fs');
@@ -9,6 +12,21 @@ var arrayify = require('arrayify-compact');
 var extend = _.extend;
 
 
+/**
+ * ```js
+ * var loader = require('load-templates');
+ * ```
+ * @param {Object} `options` Options to initialize `loader` with
+ *     @option {String} [option] `cwd` Current working directory to use for file paths.
+ *     @option {Function} [option] `parse` Function to use for parsing templates.
+ *     @option {Function} [option] `rename` Renaming function to use on the `key` of each template loaded, `path.basename` is the default.
+ *     @option {Function} [option] `normalize` Function to use for normalizing `file` objects before they are returned.
+ *     @option {Boolean} [option] `norename` Set to `true` to disable the default `rename` function.
+ *     @option {Boolean} [option] `noparse` Set to `true` to disable the default `parse` function.
+ *     @option {Boolean} [option] `nonormalize` Set to `true` to disable the default `normalize` function.
+ * @api public
+ */
+
 function loader(options) {
   loader.options = extend({
     cwd: process.cwd()
@@ -16,9 +34,54 @@ function loader(options) {
   return loader;
 }
 
-loader.options = {};
-loader.rootProps = ['data', 'content', 'path', 'orig'];
 
+/**
+ * Options cache.
+ *
+ * @type {Object}
+ */
+
+loader.options = {};
+
+
+/**
+ * Properties expected on the root of a normalized `file` object.
+ *
+ * @type {Array}
+ */
+
+loader.rootProps = ['data', 'locals', 'content', 'path', 'orig'];
+
+
+/**
+ * Expand glob patterns, load, read, parse and normalize files from
+ * file paths, strings, objects, or arrays of these types.
+ *
+ * **Examples:**
+ *
+ * Filepaths or arrays of glob patterns.
+ *
+ * ```js
+ * var temlates = loader.load(['pages/*.hbs']);
+ * var posts = loader.load('posts/*.md');
+ * ```
+ *
+ * As strings or objects:
+ *
+ * ```js
+ * // loader.load(key, value, locals);
+ * var docs = loader.load({'foo/bar.md': {content: 'this is content.'}}, {foo: 'bar'});
+ *
+ * // loader.load(key, value, locals);
+ * var post = loader.load('abc.md', 'My name is <%= name %>.', {name: 'Jon Schlinkert'});
+ * ```
+ *
+ * @param {String|Object|Array} `key` Array, object, string or file paths.
+ * @param {String|Object} `value` String of content, `file` object with `content` property, or `locals` if the first arg is a file path.
+ * @param {Object} `options` Options or `locals`.
+ * @return {Object} Normalized file object.
+ * @api public
+ */
 
 loader.load = function (key, value, options) {
   var method = loader[typeOf(key)];
@@ -27,6 +90,17 @@ loader.load = function (key, value, options) {
   }
 };
 
+
+/**
+ * Expand glob patterns, load, read, parse and normalize files
+ * from file paths or strings.
+ *
+ * @param  {String} `key` Glob patterns or file paths.
+ * @param  {String|Object} `value` String of content, `file` object with `content` property, or `locals` if the first arg is a file path.
+ * @param {Object} `options` Options or `locals`.
+ * @return {Object} Normalized file object.
+ * @api public
+ */
 
 loader.string = function (key, value, options) {
   var args = [].slice.call(arguments).filter(Boolean);
@@ -41,8 +115,7 @@ loader.string = function (key, value, options) {
     });
 
     files = glob.sync(patterns, {nonull: false});
-
-    if (files.length) {
+    if (files.length > 0) {
       files.forEach(function (filepath) {
         var key = loader.rename(filepath);
         file[key] = loader.parse(filepath);
@@ -50,13 +123,23 @@ loader.string = function (key, value, options) {
         file[key].data = extend({}, file[key].data, value);
       });
     } else {
-      file[key] = value;
+      file[key] = value || {};
     }
   }
 
   // The object should be parsed and key renamed.
   return loader.object(file, options);
 };
+
+
+/**
+ * Normalize an array of patterns.
+ *
+ * @param  {Object} `patterns` Glob patterns or array of filepaths.
+ * @param  {Object} `options` Options or `locals`
+ * @return {Array}  Array of normalized file objects.
+ * @api public
+ */
 
 loader.array = function (patterns, options) {
   var o = {};
@@ -66,17 +149,40 @@ loader.array = function (patterns, options) {
   return o;
 };
 
+
+/**
+ * Normalize a template object.
+ *
+ * @param  {Object} `file` The object to normalize.
+ * @param  {Object} `options` Options or `locals`
+ * @api public
+ */
+
 loader.object = function (file, options) {
-  if (file.hasOwnProperty('path')) {
-    file[file.path] = file;
-  }
   return this.normalize(file, options);
 };
+
+
+/**
+ * The current working directory to use. Default is `process.cwd()`.
+ *
+ * @param  {String} `filepath`
+ * @api public
+ */
 
 loader._cwd = function (filepath) {
   var cwd = path.resolve(this.options.cwd);
   return path.join(cwd, filepath);
 };
+
+
+/**
+ * Rename the `key` of each template loaded using whatever rename function
+ * is defined on the options. `path.basename` is the default.
+ *
+ * @param  {String} `filepath`
+ * @api public
+ */
 
 loader.rename = function (filepath) {
   if (this.options.rename) {
@@ -84,6 +190,16 @@ loader.rename = function (filepath) {
   }
   return filepath;
 };
+
+
+/**
+ * Parse the content of each template loaded using whatever parsing function
+ * is defined on the options. `fs.readFileSync` is used by default.
+ *
+ * @param  {String} `filepath` The path of the file to read/parse.
+ * @param  {Object} `Options` Options or `locals`.
+ * @api public
+ */
 
 loader.parse = function (filepath, options) {
   var remove = _.keys(this.options).concat('normalized');
@@ -104,6 +220,16 @@ loader.parse = function (filepath, options) {
   return o;
 };
 
+
+/**
+ * Normalize a template using whatever normalize function is
+ * defined on the options.
+ *
+ * @param  {Object} `file` The template object to normalize.
+ * @param  {Object} `Options` Options or `locals`.
+ * @api public
+ */
+
 loader.normalize = function (file, options) {
   var remove = _.keys(this.options).concat('normalized');
   var opts = _.extend({}, this.options, options);
@@ -119,10 +245,12 @@ loader.normalize = function (file, options) {
 
   _.forIn(file, function (value, key) {
     value.path = value.path || key;
+
     if (!value.hasOwnProperty('normalized')) {
       key = loader.rename(key);
       delete value.normalized;
     }
+
     var root = _.pick(value, loader.rootProps);
     root.data = extend({}, data, value.data, _.omit(value, loader.rootProps));
     o[key] = root;
@@ -132,6 +260,13 @@ loader.normalize = function (file, options) {
 };
 
 
+/**
+ * Get the type of an object.
+ *
+ * @param  {*} value
+ * @return {*}
+ * @api private
+ */
 
 function typeOf(value) {
   return Object.prototype.toString.call(value).toLowerCase()
