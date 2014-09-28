139b0c4bd8a7cb966aa0c58fcf3c2f6d7c5d5a8e
jonschlinkert <dev@sellside.com>
2014-09-28 10:38:02 -0400

    update example

--- example.js
+++ example.js
@@ -3,89 +3,101 @@
 var util = require('util');
 var path = require('path');
 var extend = require('mixin-deep');
-var normalize = require('./');
+var loader = require('./');
 
+
+/**
+ * Example application using load-templates
+ */
+
+
+
+
+/**
+ * ```js
+ * var Engine = require('engine');
+ * var engine = new Engine();
+ * ```
+ *
+ * @param {[type]} options
+ */
 
 function Engine(options) {
   this.options = options || {};
   this.cache = {};
-  this.defaultTemplates(this.options);
+  this.defaultTemplates();
 }
 
-Engine.prototype.defaultTemplates = function (options) {
-  this.create('partial', 'partials', options);
-  this.create('layout', 'layouts', options);
-  this.create('page', 'pages', options);
+/**
+ * Add some default template "types"
+ */
+
+Engine.prototype.defaultTemplates = function () {
+  this.create('partial', 'partials');
+  this.create('layout', 'layouts');
+  this.create('page', 'pages');
 };
 
-Engine.prototype.normalize = function () {
+/**
+ * Load templates with default options.
+ */
+
+Engine.prototype.load = function () {
   var opts = extend({}, this.options);
-  return normalize(opts).apply(null, arguments);
+  return loader(opts).apply(null, arguments);
 };
 
-Engine.prototype.create = function (type, plural, options) {
+/**
+ * Create template "types"
+ *
+ * @param  {String} `type` The singular name of the type, e.g. `page`
+ * @param  {String} `plural` The plural name of the type, e.g. `pages.
+ * @return {String}
+ */
+
+Engine.prototype.create = function (type, plural) {
   this.cache[plural] = this.cache[plural] || {};
 
   Engine.prototype[type] = function (key, value, locals, options) {
-    this[plural](key, value, locals, options);
+    return this[plural](key, value, locals, options);
   };
 
   Engine.prototype[plural] = function (key, value, locals, options) {
-    var files = this.normalize(key, value, locals, options);
+    var files = this.load(key, value, locals, options);
     extend(this.cache[plural], files);
     return this;
   };
-
   return this;
 };
+
+/**
+ * Create an instance of `Engine`
+ */
 
 var engine = new Engine({
   renameKey: function (filepath) {
     return path.basename(filepath);
-  },
-  // parseFn: function (filepath) {
-  //   return matter.read(filepath);
-  // },
+  }
 });
 
 
-engine.page('fixtures/two/*.md', {name: 'Brian Woodward'});
-engine.page('test/fixtures/three/*.md', {name: 'Brian Woodward'});
+/**
+ * Load some seriously disorganized templates
+ */
+
+engine.pages('fixtures/two/*.md', {name: 'Brian Woodward'});
+engine.pages('test/fixtures/three/*.md', {name: 'Brian Woodward'});
 engine.page('foo1.md', 'This is content', {name: 'Jon Schlinkert'});
 engine.page({'bar1.md': {path: 'a/b/c.md', name: 'Jon Schlinkert'}});
 engine.page({'baz.md': {path: 'a/b/c.md', name: 'Jon Schlinkert'}}, {go: true});
-engine.page(['test/fixtures/a.txt'], {name: 'Brian Woodward'});
-engine.page('test/fixtures/*.md', 'flflflfl', {name: 'Brian Woodward'});
-engine.page('layouts/*.txt', {name: 'Brian Woodward'});
-engine.page('layouts/*.txt', 'flflflfl', {name: 'Brian Woodward'});
-
-engine.page('test/fixtures/a.md', {foo: 'bar'});
+engine.pages(['test/fixtures/a.txt'], {name: 'Brian Woodward'});
+engine.pages('test/fixtures/*.md', 'flflflfl', {name: 'Brian Woodward'});
+engine.pages('pages/a.md', 'This is content.', {name: 'Jon Schlinkert'});
+engine.layout('layouts/*.txt', {name: 'Brian Woodward'});
+engine.layout('layouts/*.txt', 'flflflfl', {name: 'Brian Woodward'});
+engine.pages('test/fixtures/a.md', {foo: 'bar'});
 engine.page('abc.md', 'This is content.', {name: 'Jon Schlinkert'});
-engine.page('pages/a.md', 'This is content.', {name: 'Jon Schlinkert'});
-engine.page({'foo/bar.md': {content: 'this is content.', data: {a: 'a'}}});
-engine.page({path: 'one/two.md', content: 'this is content.', data: {b: 'b'}});
+engine.partial({'foo/bar.md': {content: 'this is content.', data: {a: 'a'}}});
+engine.partial({path: 'one/two.md', content: 'this is content.', data: {b: 'b'}});
 
-var cache = util.inspect(engine, null, 10);
-console.log(cache);
-
-
-// var templates = [];
-
-// templates.push(normalize('fixtures/two/*.md', {name: 'Brian Woodward'}));
-// templates.push(normalize('test/fixtures/three/*.md', {name: 'Brian Woodward'}));
-// templates.push(normalize('foo1.md', 'This is content', {name: 'Jon Schlinkert'}));
-// templates.push(normalize({'bar1.md': {path: 'a/b/c.md', name: 'Jon Schlinkert'}}));
-// templates.push(normalize({'baz.md': {path: 'a/b/c.md', name: 'Jon Schlinkert'}}, {go: true}));
-// templates.push(normalize(['test/fixtures/a.txt'], {name: 'Brian Woodward'}));
-// templates.push(normalize('test/fixtures/*.md', 'flflflfl', {name: 'Brian Woodward'}));
-// templates.push(normalize('layouts/*.txt', {name: 'Brian Woodward'}));
-// templates.push(normalize('layouts/*.txt', 'flflflfl', {name: 'Brian Woodward'}));
-
-// templates.push(normalize('test/fixtures/a.md', {foo: 'bar'}));
-// templates.push(normalize('abc.md', 'This is content.', {name: 'Jon Schlinkert'}));
-// templates.push(normalize('pages/a.md', 'This is content.', {name: 'Jon Schlinkert'}));
-// templates.push(normalize({'foo/bar.md': {content: 'this is content.', data: {a: 'a'}}}));
-// templates.push(normalize({path: 'one/two.md', content: 'this is content.', data: {b: 'b'}}));
-
-// var cache = util.inspect(templates, null, 10);
-// console.log(cache);
+console.log(util.inspect(engine, null, 10));
